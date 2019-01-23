94dc240d420db47ce4d7a0cfc766b711901da200
Jose Carlos Correa Mandujano <carloscorrea.dev@gmail.com>
2019-01-23 13:12:10 -0600

    user model

---
+++ bin/User.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const passportLocalMongoose =require('passport-local-mongoose');
+
+const userSchema = new Schema({
+  username: String,
+  password: String
+}, {
+  timestamps: { 
+    createdAt: "created_at",
+    updatedAt: "updated_at"
+  }
+});
+
+userSchema.plugin(passportLocalMongoose, {usernameField: "username"});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
